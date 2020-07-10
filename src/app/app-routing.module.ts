6bd87d50f6d9b79846c033426c39e9afff338b6f
Jorge A <jorge.developer@gmail.com>
2020-07-10 16:02:03 -0500

    prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddMovieComponent } from './components/add-movie/add-movie.component';
+import { EditMovieComponent } from './components/edit-movie/edit-movie.component';
+import { ListMovieComponent } from './components/list-movie/list-movie.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'movie-list' },
+  { path: 'add-movie', component: AddMovieComponent },
+  { path: 'edit-movie/:id', component: EditMovieComponent },
+  { path: 'movie-list', component: ListMovieComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddMovieComponent } from './components/add-movie/add-movie.component';
+import { EditMovieComponent } from './components/edit-movie/edit-movie.component';
+import { ListMovieComponent } from './components/list-movie/list-movie.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatDialogModule} from "@angular/material/dialog";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddMovieComponent,
+    EditMovieComponent,
+    ListMovieComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
